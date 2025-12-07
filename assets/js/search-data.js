368de3213c4f763df81b019a3098522420f68cee
indrabishnoi <153695904+indrabishnoi@users.noreply.github.com>
2025-12-07 01:17:20 +0000

    Deploying to gh-pages from @ indrabishnoi/indrabishnoi.github.io@15d1b5f470d57e8f6ec99b722c4c3fa3467ed64e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,9 +35,9 @@ ninja.data = [{
           description: "The effects of lipopolysaccharide (LPS) on conditioned disgust. Expanding the rodent model of lithium chloride (LiCl)-induced anticipatory nausea.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-maternal-preference-in-post-weaning-mice",
-          title: 'Maternal Preference in Post-Weaning Mice',
-          description: "Investigating resilient maternal preference in juvenile mice using the Agora Maze SocioBox method, examining how social bonds persist after weaning and their resilience to immune activation.",
+            },},{id: "projects-maternal-preference-in-adolescence",
+          title: 'Maternal Preference in Adolescence',
+          description: "Investigating resilient maternal preference in adolescence using the Agora Maze SocioBox method.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{
